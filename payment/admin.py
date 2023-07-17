b6cae965e28cfcf94988b2041d4bbbfbdbce7e35
taku820801 <takuchanpower@gmail.com>
2023-07-17 09:47:18 +0900

    一旦コミット

--- payment/admin.py
+++ payment/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Product, Price, Transaction  # models.pyで作成したマスタを読込
 
+
 class PriceInlineAdmin(admin.TabularInline):
     model = Price
     extra = 0
 
+
 class ProductAdmin(admin.ModelAdmin):
     inlines = [PriceInlineAdmin]
+
 
 # 管理画面に商品マスタと価格マスタを表示
 admin.site.register(Product, ProductAdmin)
--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings            # settings.pyの変数
+from django.conf import settings  # settings.pyの変数
 from django.conf.urls.static import static  # メディア表示
 
 from payment.views import (
@@ -9,15 +9,16 @@ from payment.views import (
     SuccessPageView,
     CancelPageView,
     stripe_webhook,
-    )
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),                                                                               # 管理画面
-    path("", ProductTopPageView.as_view(), name="product-top-page"),                                               # 商品トップ
-    path("create-checkout-session/<pk>/", CreateCheckoutSessionView.as_view(), name="create-checkout-session"),    # 個別商品決済画面
-    path("success/", SuccessPageView.as_view(), name="success"),                                                   # 決済成功時にリダイレクト先
-    path("cancel/", CancelPageView.as_view(), name="cancel"),                                                      # 決済キャンセル時のリダイレクト先
-    path("webhook/", stripe_webhook, name="webhook"),                                                              # 追加 Webhook
+    path('admin/', admin.site.urls),  # 管理画面
+    path("", ProductTopPageView.as_view(), name="product-top-page"),  # 商品トップ
+    path("create-checkout-session/<pk>/", CreateCheckoutSessionView.as_view(), name="create-checkout-session"),
+    # 個別商品決済画面
+    path("success/", SuccessPageView.as_view(), name="success"),  # 決済成功時にリダイレクト先
+    path("cancel/", CancelPageView.as_view(), name="cancel"),  # 決済キャンセル時のリダイレクト先
+    path("webhook/", stripe_webhook, name="webhook"),  # 追加 Webhook
 ]
 
 # メディア表示
